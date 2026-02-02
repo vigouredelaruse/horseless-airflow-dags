41f5005a865e109287b68b3d4f628344189fc3cf
devopswizard <devops@wizard.com>
2026-02-02 01:12:56 -0500

    extra url updates

--- dags/sentinel_toa.py
+++ dags/sentinel_toa.py
@@ -96,9 +96,9 @@ with DAG(
         requirements=[
                 'pystac-client',
                 'odc-stac',
-                'pendulum',
-                    '--extra-index-url https://pkgs.dev.azure.com/wizardcontroller/MetOffice/_apis/packaging/feeds/29c04fda-7517-4d3b-872e-1134a0ecf4da/pypi/simple/',
-                    'horseless-atmospheric-correction==0.0.2'],
+                'pendulum',  
+                'horseless-atmospheric-correction==0.0.2'],
+        index_urls=['https://pkgs.dev.azure.com/wizardcontroller/MetOffice/_apis/packaging/feeds/29c04fda-7517-4d3b-872e-1134a0ecf4da/pypi/simple/'],
         system_site_packages=False,
         # Pass a templated `ingest` dict; runs may override via DagRun.conf['ingest']
         op_kwargs={
