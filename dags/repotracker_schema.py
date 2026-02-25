d1346228834a06b30374c547450a48ca8cbca56b
dev ops <d@evops.com>
2026-02-24 21:16:09 -0500

    redis messagetrigger update

--- dags/repotracker_schema.py
+++ dags/repotracker_schema.py
@@ -18,7 +18,7 @@ _SCHEMA_RESET_CHANNEL = Variable.get("REDIS_PUBSUB_SCHEMAOPS_RESET_CHANNEL", def
 schema_reset_trigger = MessageQueueTrigger(
     scheme="redis+pubsub",
     channels=[_SCHEMA_RESET_CHANNEL],
-    redis_conn_id="schema_reset",
+    conn_id="schema_reset",
 )
 
 schema_reset_asset = Asset(
