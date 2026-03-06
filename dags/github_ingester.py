68a3aa00c2b622b79b00928aa81826d5d3b8dd25
dev ops <d@evops.com>
2026-03-06 14:50:11 -0500

    dag updates

--- dags/github_ingester.py
+++ dags/github_ingester.py
@@ -130,7 +130,7 @@ def github_ingester():
 
     @task.kubernetes(
         task_id="persist_model_run",
-        image="localhost:32000/horseless-repotracker:latest",
+        image="docker-registry.dubridge.ataxlab.com/horseless-repotracker@sha256:3e7706e7709beb835664e613a72d3215c529013c16ab2b2f3c2dbefa4eef2bb0",
         name="k8s-env-task",
         env_vars=_VENV_ENV_VARS,
         image_pull_policy="IfNotPresent", 
@@ -273,7 +273,7 @@ def github_ingester():
 
     @task.kubernetes(
         task_id="ingest_repositories",
-        image="localhost:32000/horseless-repotracker:latest",
+        image="docker-registry.dubridge.ataxlab.com/horseless-repotracker@sha256:3e7706e7709beb835664e613a72d3215c529013c16ab2b2f3c2dbefa4eef2bb0",
         name="k8s-env-task",
         env_vars=_VENV_ENV_VARS,
         image_pull_policy="IfNotPresent", 
@@ -410,7 +410,7 @@ def github_ingester():
 
     @task.kubernetes(
         task_id="ingest_issues",
-        image="localhost:32000/horseless-repotracker:latest",
+        image="docker-registry.dubridge.ataxlab.com/horseless-repotracker@sha256:3e7706e7709beb835664e613a72d3215c529013c16ab2b2f3c2dbefa4eef2bb0",
         name="k8s-env-task",
         env_vars=_VENV_ENV_VARS,
         image_pull_policy="IfNotPresent",
@@ -542,7 +542,7 @@ def github_ingester():
     
     @task.kubernetes(
         task_id="refresh_materialized_views",
-        image="localhost:32000/horseless-repotracker:latest",
+        image="docker-registry.dubridge.ataxlab.com/horseless-repotracker@sha256:3e7706e7709beb835664e613a72d3215c529013c16ab2b2f3c2dbefa4eef2bb0",
         name="k8s-env-task",
         env_vars=_VENV_ENV_VARS,
         image_pull_policy="IfNotPresent",
@@ -611,7 +611,7 @@ def github_ingester():
 
     @task.kubernetes(
         task_id="publish_enrichment_trigger",
-        image="localhost:32000/horseless-repotracker:latest",
+        image="docker-registry.dubridge.ataxlab.com/horseless-repotracker@sha256:3e7706e7709beb835664e613a72d3215c529013c16ab2b2f3c2dbefa4eef2bb0",
         name="k8s-env-task",
         env_vars=build_venv_env_vars(include_redis=True),
         image_pull_policy="IfNotPresent",
--- dags/repotracker_schema.py
+++ dags/repotracker_schema.py
@@ -135,7 +135,7 @@ def repotracker_schema_reset_handler():
 
     @task.kubernetes(
         task_id="create_database_if_not_exists",
-        image="thehorselessnewspaper/horseless-repotracker@sha256:ae758925e003993c1f665dbf1e8f00de7ec54dd99b0ed533a2f6d9bb5c7e2ad1",
+        image="docker-registry.dubridge.ataxlab.com/horseless-repotracker@sha256:3e7706e7709beb835664e613a72d3215c529013c16ab2b2f3c2dbefa4eef2bb0",
         name="k8s-env-task",
         env_vars=_VENV_ENV_VARS,
         image_pull_policy="IfNotPresent",
@@ -180,7 +180,7 @@ def repotracker_schema_reset_handler():
 
     @task.kubernetes(
         task_id="drop_and_recreate_schema",
-        image="thehorselessnewspaper/horseless-repotracker@sha256:ae758925e003993c1f665dbf1e8f00de7ec54dd99b0ed533a2f6d9bb5c7e2ad1",
+        image="docker-registry.dubridge.ataxlab.com/horseless-repotracker@sha256:3e7706e7709beb835664e613a72d3215c529013c16ab2b2f3c2dbefa4eef2bb0",
         name="k8s-env-task",
         env_vars=_VENV_ENV_VARS,
         image_pull_policy="IfNotPresent", 
@@ -245,7 +245,7 @@ def repotracker_schema_reset_handler():
 
     @task.kubernetes(
         task_id="create_materialized_views",
-        image="thehorselessnewspaper/horseless-repotracker@sha256:ae758925e003993c1f665dbf1e8f00de7ec54dd99b0ed533a2f6d9bb5c7e2ad1",
+        image="docker-registry.dubridge.ataxlab.com/horseless-repotracker@sha256:3e7706e7709beb835664e613a72d3215c529013c16ab2b2f3c2dbefa4eef2bb0",
         name="k8s-env-task",
         env_vars=_VENV_ENV_VARS,
         image_pull_policy="IfNotPresent", 
